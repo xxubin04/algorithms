34d8e39def21173ca13543a64b563c6a2d9db9f6
정유빈 <127945869+xxubin04@users.noreply.github.com>
2023-11-21 23:55:34 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1372 ms, Memory: 109940 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -1,25 +1,24 @@
 from sys import stdin
 
 score = 0; stack = []
-N = int(stdin.readline())
 
-for n in range(1, N+1):
+N = int(stdin.readline())
+for n in range(N):
     aList = list(map(int, stdin.readline().split()))
     if aList[0] == 1:
-        A = aList[1]; T = aList[2]
-        stack.append([n, A, T])
-        if stack[-1][-1] == 1:
-            score += stack[-1][1]
-            stack.pop(-1)
+        if aList[2] == 1:
+            score += aList[1]
+            aList.pop()
         else:
-            stack[-1][-1] -= 1
+            aList[2] -= 1
+            stack.append(aList)
     else:
         if not stack:
             pass
         else:
-            if stack[-1][-1] == 1:
+            if stack[-1][2] == 1:
                 score += stack[-1][1]
                 stack.pop(-1)
             else:
-                stack[-1][-1] -= 1
+                stack[-1][2] -= 1
 print(score)
