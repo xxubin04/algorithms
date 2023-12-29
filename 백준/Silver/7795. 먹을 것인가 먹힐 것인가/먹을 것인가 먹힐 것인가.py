e337cb4b30d5f9319386dfd0eea8676761eea19d
정유빈 <127945869+xxubin04@users.noreply.github.com>
2023-12-29 10:06:06 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 140 ms, Memory: 34488 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -5,14 +5,14 @@ for _ in range(test):
     A_num, B_num = map(int, stdin.readline().split())
     A = list(map(int, stdin.readline().split()))
     B = list(map(int, stdin.readline().split()))
-    A.sort(reverse=True); B.sort(reverse=True)
-    start = 0; pair = 0
-    A_index = 0; B_index = 0
+    A.sort()
+    B.sort()
+    pair = 0
+    B_index = 0
 
-    while A_index < A_num and B_index < B_num:
-        if A[A_index] > B[B_index]:
-            pair += B_num - B_index
-            A_index += 1
-        else:
+    for a in A:
+        while B_index < B_num and B[B_index] < a:
             B_index += 1
+        pair += B_index
+
     print(pair)
