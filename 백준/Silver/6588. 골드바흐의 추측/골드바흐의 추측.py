48be3400fe7c49a9e56eb028ef0205b200ed6d66
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-01-19 12:31:27 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2776 ms, Memory: 46748 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -2,15 +2,20 @@ input = open(0).readline
 
 prime = [0, 0] + [1] * 999999
 for j in range(2, 1001):
-    if prime[j] == 1:
+    if prime[j]:
         for k in range(j*2, 1000001, j):
             prime[k] = 0
 prime[2] = 0
 
-num = int(input())
-while num != 0:
-    for i in range(len(prime)):
-        if prime[i] == 1 and prime[num - i] == 1:
-            print(f"{num} = {i} + {num-i}")
-            break
+while True:
+    flag = False
     num = int(input())
+    if num == 0:
+        break
+    for i in range(3, num//2+1, 2):
+        if prime[i] and prime[num-i]:
+            print(f"{num} = {i} + {num-i}")
+            flag = True
+            break
+    if flag == False:
+        print("Goldbach's conjecture is wrong.")
