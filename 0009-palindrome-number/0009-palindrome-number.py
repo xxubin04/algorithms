38f17005918882dc7b94780baac63ac561b9a9f3
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-01-30 00:22:17 +0900

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        word = list(str(x))
-        for i in range(len(word)//2+1):
-            if word[i] == word[-(i+1)]:
-                continue
-            else:
-                return False
-        return True
