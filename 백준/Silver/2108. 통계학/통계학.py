83e38a757bf99dbc4c31963ed1176a6f546830d6
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-01-31 11:13:00 +0900

    [Silver III] Title: 통계학, Time: 504 ms, Memory: 56412 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,16 +1,16 @@
-from collections import Counter
 input = open(0).readline
 
 cnt = int(input())
 num = sorted([int(input()) for i in range(cnt)])
 print(round(sum(num)/cnt))
 print(int(num[cnt//2] if cnt%2 != 0 else sum(num[cnt//2-1:cnt//2+1])/2))
-frq = Counter(num)
-frq_cnt = 0
-for k, v in frq.items():
-    if v == max(frq.values()): frq_cnt += 1
-if frq_cnt >= 2:
-    print(sorted(frq.items(), reverse=True, key=lambda x:x[1])[1][0])
-else:
-    print(sorted(frq.items(), reverse=True, key=lambda x:x[1])[0][0])
+frq_list = {}
+for j in num:
+    if j in frq_list:
+        frq_list[j] += 1
+    else:
+        frq_list[j] = 1
+frq_list = sorted(frq_list.items(), reverse=True, key=lambda x:x[1])
+frq_values = [k for k, v in frq_list if v == frq_list[0][1]]
+print(frq_list[1][0] if len(frq_values) >= 2 else frq_list[0][0])
 print(num[-1]-num[0])
