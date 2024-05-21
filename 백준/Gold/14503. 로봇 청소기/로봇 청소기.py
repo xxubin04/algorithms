a8cf16e027f7df865211d0a381a65f3df1862dfc
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-05-21 20:02:49 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31244 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -14,17 +14,15 @@ def now_clean(x, y, d):
 def around_clean(x, y, d):
     for _ in range(4):
         d = (d + 3) % 4  # 반시계로 90도 회전
-        nx, ny = x + dx[d], y + dy[d]
-        if room[nx][ny] == 0:  # 앞의 빈칸이 청소 X
-            now_clean(nx, ny, d)
+        if room[x + dx[d]][y + dy[d]] == 0:  # 앞의 빈칸이 청소 X
+            now_clean(x + dx[d], y + dy[d], d)
             return
     back(x, y, d)
 
 def back(x, y, d):
     bd = (d + 2) % 4  # 후진 방향
-    bx, by = x + dx[bd], y + dy[bd]
-    if room[bx][by] != 1:  # 벽이 아닌 경우 후진
-        now_clean(bx, by, d)
+    if room[x + dx[bd]][y + dy[bd]] != 1:  # 벽이 아닌 경우 후진
+        now_clean(x + dx[bd], y + dy[bd], d)
 
 N, M = map(int, input().split())
 initial_x, initial_y, initial_d = map(int, input().split())
