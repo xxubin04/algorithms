940ea678ef6b5b96be4fc79ee580f975737c9e0c
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-07-05 17:30:03 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -5,8 +5,9 @@ def solution(s):
     for i in range(2, len(s)-1):
         if s[i] == '}':
             nums.append(s[a:i].split(","))
-            a = i + 3
-            i += 2
+            i += 3
+            a = i
+            
     for n in sorted(nums, key=lambda x: len(x)):
         for m in n:
             if not int(m) in answer:
