a92758a756c38e15aa59ca95fe6f7573efe8d48e
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-01-09 11:26:05 +0900

    [Silver V] Title: 집합, Time: 3632 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,22 @@
+input = open(0).readline
+
+S = set([])
+
+for i in range(M := int(input())):
+    command = list(input().strip().split())
+    if command[0] == "add" and command[1] not in S:
+        S.add(command[1])
+    elif command[0] == "remove" and command[1] in S:
+        S.remove(command[1])
+    elif command[0] == "check" and command[1] in S:
+        print(1)
+    elif command[0] == "check" and command[1] not in S:
+        print(0)
+    elif command[0] == "toggle" and command[1] in S:
+        S.remove(command[1])
+    elif command[0] == "toggle" and command[1] not in S:
+        S.add(command[1])
+    elif command[0] == "all":
+        S = set(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
+    elif command[0] == "empty":
+        S = set([])
