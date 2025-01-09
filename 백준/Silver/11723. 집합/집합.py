3c1a6d96250a4c328f8d93c4dc8d9b1b409dd4c3
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-01-09 11:27:28 +0900

    [Silver V] Title: 집합, Time: 3504 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -8,14 +8,16 @@ for i in range(M := int(input())):
         S.add(command[1])
     elif command[0] == "remove" and command[1] in S:
         S.remove(command[1])
-    elif command[0] == "check" and command[1] in S:
-        print(1)
-    elif command[0] == "check" and command[1] not in S:
-        print(0)
-    elif command[0] == "toggle" and command[1] in S:
-        S.remove(command[1])
-    elif command[0] == "toggle" and command[1] not in S:
-        S.add(command[1])
+    elif command[0] == "check":
+        if command[1] in S:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "toggle":
+        if command[1] in S:
+            S.remove(command[1])
+        else:
+            S.add(command[1])
     elif command[0] == "all":
         S = set(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
     elif command[0] == "empty":
