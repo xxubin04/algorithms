012da0db8506b6525317e282e83a7d72a21bda31
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-01-09 20:56:09 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,17 @@
+input = open(0).readline
+
+medal = {}
+nation_num, wanna_rank = map(int, input().split())
+
+for _ in range(nation_num):
+    nation, gold, silver, bronze = map(int, input().split())
+    medal[nation] = [gold, silver, bronze]
+
+line = sorted(medal.values(), key=lambda x: (x[0], x[1], x[2]), reverse=True)
+
+line_no_dup = []
+for i in line:
+    if i not in line_no_dup:
+        line_no_dup.append(i)
+
+print(line_no_dup.index(medal[wanna_rank])+1)
