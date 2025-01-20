66541792e43e90aa53781739b2334909b2360071
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-01-20 20:34:07 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,14 @@
 def solution(participant, completion):
-    answer = ''
-    return answer
+    success = {}
+    for p in participant:
+        if p in success:
+            success[p] += 1
+        else:
+            success[p] = 1
+    
+    for c in completion:
+        success[c] -= 1
+        
+    for name, num in success.items():
+        if num != 0:
+            return name
