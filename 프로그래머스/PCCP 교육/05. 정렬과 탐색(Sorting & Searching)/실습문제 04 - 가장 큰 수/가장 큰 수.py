d36212e538ac2aaa11029ea29a50b81cf1a8703d
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-01-21 19:06:03 +0900

    Create 가장 큰 수.py

---
+++ 프로그래머스/PCCP 교육/05. 정렬과 탐색(Sorting & Searching)/실습문제 04 - 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    result = ''.join(numbers)
+    return str(int(result))
