6b504d36a884bc6812ac3369dfa842d66f8e2b42
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-02-01 11:14:33 +0900

    Create 볼링공 고르기 01.py

---
+++ 이것이 코딩 테스트다 with 파이썬/Ch.03 그리디/PART 03. 알고리즘 유형별 기출문제/Q. 05 - 볼링공 고르기/볼링공 고르기 01.py
@@ -0,0 +1,17 @@
+# 이것이 코테다 p.315
+# Ch.11 그리디
+# 기출 문제 - 05. 볼링공 고르기
+# 걸린 시간: 03분 20초 
+# 일시: 2024.02.01
+
+N, M = map(int, input().split())
+
+weights = sorted(list(map(int, input().split())))
+cnt = 0  # 무게가 다른 볼링공 조합 개수
+
+for i in range(N):
+  for j in range(i, N):
+    if weights[i] != weights[j]:  # 무게가 다른 경우에만 +1
+      cnt += 1
+
+print(cnt)
