865abaad1c33174b11e83560cfb09e9a45bbfa4f
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-02-01 10:24:03 +0900

    Create 모험가 길드 01.py

---
+++ 이것이 코딩 테스트다 with 파이썬/Ch.03 그리디/PART 03. 알고리즘 유형별 기출문제/Q. 01 - 모험가 길드/모험가 길드 01.py
@@ -0,0 +1,18 @@
+# 이것이 코테다 p.311
+# Ch.11 그리디
+# 기출 문제 - 01. 모험가 길드
+# 걸린 시간: 04분 51초 
+# 일시: 2024.02.01
+
+N = int(input())
+# 내림차순으로 공포도 정렬
+scary = sorted(list(map(int, input().split())), reverse=True)
+
+cnt = 0  # 모험을 떠나는 그룹 수
+
+while scary:  # 남은 공포도(사람)가 있다면
+  i = scary[0]  # 남은 사람들 중 가장 높은 공포도
+  scary = scary[i:]  # 공포도가 높은 사람들 i명 제거(집단 생성)
+  cnt += 1  # 집단 수 1 증가
+  
+print(cnt)
