f26dfa764968b57ab97ba7ad05a9b7d2fded1958
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-03-12 19:11:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i+1, j+1]
