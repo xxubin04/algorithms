ee97e1cdd5dc1c6f131bfe405cbd4eda583f6d99
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-03-14 10:24:42 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+res = []
+
+def backtracking():
+    if len(res) == m:
+        print(' '.join(map(str, res)))
+        return
+
+    for i in range(1, n+1):
+        if i not in res:
+            res.append(i)
+            backtracking()
+            res.pop()
+
+backtracking()
