74f5f52b7e07822ddb3361fcf536dffb1f26c3be
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-05-29 10:48:59 +0900

    [Gold V] Title: 탑, Time: 644 ms, Memory: 131116 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,17 @@
+input = open(0).readline
+
+n = int(input())
+towers = list(map(int, input().split()))
+stk =  [(0, towers[0])]
+answer = [0] * n
+
+for t in range(1, n):
+    while stk:
+        if towers[t] <= stk[-1][1]:
+            answer[t] = stk[-1][0] + 1
+            break
+        else:
+            stk.pop()
+    stk.append((t, towers[t]))
+
+print(*answer)
