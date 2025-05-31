3b93f2f07d6ea5a71fb6a0939c71f4390aefb88c
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-05-31 09:13:00 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+stk = deque()
+
+for i in range(n := int(sys.stdin.readline())):
+    cmd = list(sys.stdin.readline().split())
+
+    if cmd[0] == 'push':
+        stk.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if len(stk) == 0:
+            print(-1)
+        else:
+            print(stk.popleft())
+    elif cmd[0] == 'size':
+        print(len(stk))
+    elif cmd[0] == 'empty':
+        if len(stk) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'front':
+        if len(stk) == 0:
+            print(-1)
+        else:
+            print(stk[0])
+    elif cmd[0] == 'back':
+        if len(stk) == 0:
+            print(-1)
+        else:
+            print(stk[-1])
