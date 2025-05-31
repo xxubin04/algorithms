779c5e1e0d400db3f0843c6ee2d56c393874bb26
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-05-31 15:18:09 +0900

    [Silver II] Title: 스택 수열, Time: 180 ms, Memory: 35020 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,36 +1,21 @@
 import sys
 
-stk = [1]
-not_yet_push = 2  # 아직 스택에 넣지 않은 수 중에서 최솟값
-op = ['+']
-flag = True
+op = []
+stk = []
+next_num = 1
 
-for i in range((n := int(sys.stdin.readline()))):
-    num = int(sys.stdin.readline())
-
-    if len(stk) == 0 and not_yet_push <= num:
-        stk.append(not_yet_push)
+for i in range(n := int(sys.stdin.readline())):
+    target = int(sys.stdin.readline())
+    while next_num <= target:
+        stk.append(next_num)
         op.append('+')
-        not_yet_push += 1
-    elif len(stk) == 0 and not_yet_push > num:
+        next_num += 1
+
+    if stk[-1] == target:
+        stk.pop()
+        op.append('-')
+    else:
         print("NO")
         exit()
-
-    while stk:
-        if stk[-1] == num:
-            stk.pop()
-            op.append('-')
-            break
-        elif stk[-1] > num:
-            stk.pop()
-            op.append('-')
-        elif stk[-1] < num:  # 스택의 top이 pop하고자 하는 수보다 작은 경우
-            if not_yet_push > num :  # 아직 넣지 않은 수들 중 최솟값이 pop하고자 하는 수보다 큰 경우
-                print("NO")
-                exit()
-            else:  # 아직 넣지 않은 수들 중 회솟값이 pop하고자 하는 수보다 작은 경우
-                stk.append(not_yet_push)
-                op.append('+')
-                not_yet_push += 1
 
 for p in op: print(p)
