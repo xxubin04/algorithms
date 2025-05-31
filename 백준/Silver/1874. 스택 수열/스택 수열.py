2521f528481bc5048e00133f4c2a39f8dc27de98
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-05-31 13:54:08 +0900

    [Silver II] Title: 스택 수열, Time: 212 ms, Memory: 35020 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,39 @@
+import sys
+
+stk = [1]
+not_yet_push = 2
+op = ['+']
+flag = True
+
+for i in range((n := int(sys.stdin.readline()))):
+    num = int(sys.stdin.readline())
+
+    if len(stk) == 0 and not_yet_push <= num:
+        stk.append(not_yet_push)
+        op.append('+')
+        not_yet_push += 1
+    elif len(stk) == 0 and not_yet_push > num:
+        print("NO")
+        exit()
+
+    while stk:
+        if stk[-1] == num:
+            stk.pop()
+            op.append('-')
+            break
+        elif stk[-1] > num:
+            stk.pop()
+            op.append('-')
+        elif stk[-1] < num:  # 스택의 top이 pop하고자 하는 수보다 작은 경우
+            if not_yet_push > num :  # 아직 넣지 않은 수들 중 최솟값이 pop하고자 하는 수보다 큰 경우
+                print("NO")
+                exit()
+            else:  # 아직 넣지 않은 수들 중 회솟값이 pop하고자 하는 수보다 작은 경우
+                stk.append(not_yet_push)
+                op.append('+')
+                not_yet_push += 1
+
+if not flag:
+    print("NO")
+else:
+    for p in op: print(p)
