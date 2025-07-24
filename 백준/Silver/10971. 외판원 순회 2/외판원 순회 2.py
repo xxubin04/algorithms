bb65b84c653961a5660b5c36015e8ce023f73859
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-07-24 12:00:55 +0900

    [Silver II] Title: 외판원 순회 2, Time: 556 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,28 +1,38 @@
 import sys
-sys.setrecursionlimit(10**6)
 
-w = []
+dist = []  # 도시 간의 거리 저장 
 
-def tsp(i, visited, cost):
-    min_cost = float('inf')
-
-    if all(visited):
-        if w[i][0] != 0:
-            return cost + w[i][0]
-        else:
+def shortestPath(path, visited, currentLength):
+    if len(path) == n:  # 모든 도시를 순회했다면 
+        if dist[path[-1]][path[0]] == 0:  # 갈 수 없는 경로라면 
             return float('inf')
+        return currentLength + dist[path[-1]][path[0]]
 
-    for j in range(1, n):
-        if visited[j] == 0 and w[i][j] != 0:
-            visited[j] = 1
-            min_cost = min(min_cost, tsp(j, visited, cost + w[i][j]))
-            visited[j] = 0
+    ret = float('inf')
 
-    return min_cost
+    for next in range(n):
+        if visited[next]:  # 이미 방문한 도시라면 패스 
+            continue
+
+        if dist[path[-1]][next] == 0:  # 갈 수 없는 경로라면 패스
+            continue
+
+        here = path[-1]  # 현재 위치한 도시 
+        path.append(next)  # 다음 방문한 도시를 경로에 추가 
+        visited[next] = True  # 방문 처리 
+
+        cand = shortestPath(path, visited, currentLength + dist[here][next])
+
+        ret = min(ret, cand)  # 최소 거리 갱신 
+        visited[next] = False  # 방문 미처리 
+        path.pop()  # 경로에서 해당 도시 뺴냄 (백트래킹)
+
+    return ret
 
 for _ in range(n := int(sys.stdin.readline().strip())):
-    w.append(list(map(int, sys.stdin.readline().strip().split())))
+    dist.append(list(map(int, sys.stdin.readline().strip().split())))
 
-visited = [1] + [0] * (n-1)
+visited = [True] + [False] * (n-1)
+path = [0]  # 경로에 시작 도시로 초기화
 
-print(tsp(0, visited, 0))
+print(shortestPath(path, visited, 0))
