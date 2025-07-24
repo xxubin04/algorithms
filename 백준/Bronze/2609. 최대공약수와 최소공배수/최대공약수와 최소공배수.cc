0e5b870c6e9989a16930ee7c2773d5a7ca7cdb68
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-07-25 08:43:43 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,20 +1,13 @@
-#include <iostream>
+#include <iostream>  
+#include <numeric>  
 
-using namespace std;
+using namespace std;  
 
-int gcd(int a, int b) {
-	return b == 0 ? a : gcd(b, a % b);
-}
+int main() {  
+	int a, b;  
+	cin >> a >> b;  
 
-int lcm(int a, int b) {
-	return a * b / gcd(a, b);
-}
-
-int main() {
-	int a, b;
-	cin >> a >> b;
-
-	cout << gcd(a, b) << '\n';
+	cout << gcd(a, b) << '\n';  
 	cout << lcm(a, b) << '\n';
 
 	return 0;
