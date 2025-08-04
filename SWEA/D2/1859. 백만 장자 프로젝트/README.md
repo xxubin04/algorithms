64e2f7178c7fd23a7d135441b9f9c75642e16c82
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-08-04 13:04:24 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 736 ms, Memory: 9,816 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/README.md
@@ -0,0 +1,15 @@
+# [D2] 백만 장자 프로젝트 - 1859 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc) 
+
+### 성능 요약
+
+메모리: 9,816 KB, 시간: 736 ms, 코드길이: 470 Bytes
+
+### 제출 일자
+
+2025-08-04 13:04
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int tc;
+
+int main() {
+	int cnt = 0;
+	cin >> tc;
+
+	while (true) {
+		if (cnt == tc) break;
+
+		cnt++;
+		int n;
+		cin >> n;
+
+		vector<int> v(n, 0);
+
+		for (int i = 0; i < n; i++)
+			cin >> v[i];
+
+		int max_price = 0;
+        ll ans = 0;
+		for (int j = n - 1; j >= 0; j--) {
+			if (v[j] >= max_price)
+				max_price = v[j];
+			ans += (max_price - v[j]);
+		}
+		cout << '#' << cnt << ' ' << ans << '\n';
+	}
+}
