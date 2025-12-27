1dbdd1a6b15b7da658c2f4aa697e29267c93cfa1
xxubin <xxubin04@gmail.com>
2025-12-27 12:27:42 +0900

    chore:remove broken codetree directory

--- codetree/251226/세 수의 중간값/median-of-three-numbers.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-a, b, c = map(int, sys.stdin.readline().split())
-print(1 if (a < b < c) else 0)
--- codetree/251226/시력 검사/eye-test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-r = float(sys.stdin.readline())
-if ((l := float(sys.stdin.readline())) >= 1 and r >= 1):
-    print("High")
-elif (l >= 0.5 and r >= 0.5):
-    print("Middle")
-else:
-    print("Low")
