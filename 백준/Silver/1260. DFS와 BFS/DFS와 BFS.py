e3c39bef3e81c77b59f9a7c9b6936c48fe2d1daa
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-03-10 12:27:57 +0900

    [Silver II] Title: DFS와 BFS, Time: 140 ms, Memory: 40436 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,53 +1,35 @@
 from collections import deque
-import copy
-import sys
-sys.setrecursionlimit(10**6)
 
 input = open(0).readline
+N, M, V = map(int, input().split())
 
-num, line, start = map(int, input().split())
+graph = [[False] * (N+1) for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
 
-node = [[] for _ in range(num+1)]
-for i in range(line):
-    A, B = map(int, input().split())
-    node[A].append(B)
-    node[B].append(A)
+visited_dfs = [False] * (N+1)
+visited_bfs = [False] * (N+1)
 
-for j in range(num+1):
-    node[j].sort()
+def bfs(V):
+    q = deque([V])
+    visited_bfs[V] = True
+    while q:
+        V = q.popleft()
+        print(V, end=" ")
+        for i in range(1, N+1):
+            if not visited_bfs[i] and graph[V][i]:
+                q.append(i)
+                visited_bfs[i] = True
 
-visited_DFS = [0 for _ in range(num+1)]
-visited_BFS = [0 for _ in range(num+1)]
-visited_BFS[start] = 1
-ans_DFS = []
-ans_BFS = [start]
-proD = []
-proB = deque([start])
+def dfs(V):
+    visited_dfs[V] = True
+    print(V, end=" ")
+    for i in range(1, N+1):
+        if not visited_dfs[i] and graph[V][i]:
+            dfs(i)
 
-def DFS(x):
-    if visited_DFS[x] == 0:
-        visited_DFS[x] = 1
-        ans_DFS.append(x)
-        proD.append(node[x])
-        for d in node[x]:
-            DFS(d)
-
-def BFS(x):
-    while proB:
-        proB.popleft()
-        for c in node[x]:
-            if visited_BFS[c] == 0:
-                proB.append(c)
-                ans_BFS.append(c)
-            visited_BFS[c] = 1
-        for k in copy.deepcopy(proB):
-            BFS(k)
-
-DFS(start)
-BFS(start)
-
-for a in ans_DFS:
-    print(a, end=' ')
-print("\n", end="")
-for b in ans_BFS:
-    print(b, end=' ')
+dfs(V)
+print()
+bfs(V)
