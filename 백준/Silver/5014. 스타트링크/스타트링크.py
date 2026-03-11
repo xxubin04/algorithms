ae87210b161336ec07a2ebf4caad0f314c05fa0c
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-03-11 10:48:48 +0900

    [Silver I] Title: 스타트링크, Time: 344 ms, Memory: 41796 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,24 +1,24 @@
 from collections import deque
+input = open(0).readline
 
-input = open(0).readline()
-F, S, G, U, D = map(int, input.split())
+F, S, G, U, D = map(int, input().split())
 q = deque([(S, 0)])
-visited = [0 for _ in range(F+1)]
-visited[S] = 1
+visited = [0] * (F+1)
 
-while q:
-    floor, count = q.popleft()
+def bfs(loc):
+    visited[loc] = 1
+    while q:
+        floor, cnt = q.popleft()
+        if floor == G:
+            return cnt
 
-    if floor == G:
-        print(count)
-        break
+        if 1 <= (u := floor+U) <= F and not visited[u]:
+            visited[u] = 1
+            q.append((u, cnt+1))
+        if 1 <= (d := floor-D) <= F and not visited[d]:
+            visited[d] = 1
+            q.append((d, cnt+1))
 
-    if (up := floor + U) <= F and visited[up] == 0:
-        visited[up] = 1
-        q.append((up, count + 1))
+    return "use the stairs"
 
-    if (down := floor - D) >= 1 and visited[down] == 0:
-        visited[down] = 1
-        q.append((down, count + 1))
-else:
-    print("use the stairs")
+print(bfs(S))
