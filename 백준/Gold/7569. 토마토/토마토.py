256d0912d61a508bee3947c06d25aaf0b4471b66
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-03-11 14:23:40 +0900

    [Gold V] Title: 토마토, Time: 1648 ms, Memory: 50324 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -4,33 +4,27 @@ input = open(0).readline
 q = deque([])
 box = []
 M, N, H = map(int, input().split())
-visited = [[[0] * M for _ in range(N)] for _ in range(H)]
 
 for _ in range(H):
     layer = [list(map(int, input().split())) for _ in range(N)]
     box.append(layer)
 
 def bfs():
-    d = 0
     while q:
-        z, x, y, d = q.popleft()
+        z, x, y = q.popleft()
         for i in ((0, 0, 1), (0, 1, 0), (0, 0, -1), (0, -1, 0), (1, 0, 0), (-1, 0, 0)):
             nz, nx, ny = z + i[0], x + i[1], y + i[2]
-            if 0 <= nz < H and 0 <= nx < N and 0 <= ny < M and visited[nz][nx][ny] == 0 and (b := box[nz][nx][ny]) != -1:
-                if b == 0:  # 토마토가 익지 않은 상태라면
-                    box[nz][nx][ny] = 1  # 토마토의 상태를 익음으로 변경
-                q.append((nz, nx, ny, d+1))
-                visited[nz][nx][ny] = 1
-
-    return d
+            if 0 <= nz < H and 0 <= nx < N and 0 <= ny < M and box[nz][nx][ny] == 0:
+                box[nz][nx][ny] = box[z][x][y] + 1  # 토마토의 상태가 익었거나 익지 않은 경우 -> 일자로 저장
+                q.append((nz, nx, ny))
 
 for a in range(H):
     for b in range(N):
         for c in range(M):
-            if box[a][b][c] == 1 and not visited[a][b][c]:
-                q.append((a, b, c, 0))
-                visited[a][b][c] = 1
-day = bfs()
+            if box[a][b][c] == 1:
+                q.append((a, b, c))
+
+bfs()
 
 for i in range(H):
     for j in range(N):
@@ -38,4 +32,4 @@ for i in range(H):
             print(-1)
             exit()
 
-print(0 if day == 1 else day)
+print(0 if (day := (max(max(row) for layer in box for row in layer))) == 1 else day-1)
