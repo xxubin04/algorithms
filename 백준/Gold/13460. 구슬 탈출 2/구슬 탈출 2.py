c792bea837c7d276ad339368688a480210460626
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-03-22 14:23:31 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 60 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,8 +1,8 @@
 from collections import deque
-input = open(0).readline 
+input = open(0).readline
 
 N, M = map(int, input().split())
-board = [list(input()) for _ in range(N)]
+board = [list(input().strip()) for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
@@ -15,24 +15,21 @@ for i in range(N):
         elif board[i][j] == 'O':
             ox, oy = i, j
 
+directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
 def move(x, y, dx, dy):
-    # 구슬 하나를 방향으로 끝까지 이동, 구멍 빠지면 탈출
-    count = 0
-    while board[x + dx][y + dy] != '#':
+    count = 0  # 빨간 구슬이랑 파란 구슬 겹쳤을 때 더 많이 움직인 구슬을 옮기기 위해 이동한 칸 개수 저장하는 변수
+    while board[x + dx][y + dy] != '#':  # 벽/구멍이 아닐때까지
+        count += 1
         x += dx
         y += dy
-        count += 1
-        if x == ox and y == oy:
+        if x == ox and y == oy:  # 구멍에 빠진다면
             break
-    return x, y, count
+    return x, y, count  # 구슬의 최종 좌표와 이동횟수 반환
 
 def bfs():
     visited = set()
     visited.add((rx, ry, bx, by))
-    queue = deque()
-    queue.append((rx, ry, bx, by, 0))
-
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우
+    queue = deque([(rx, ry, bx, by, 0)])
 
     while queue:
         crx, cry, cbx, cby, cnt = queue.popleft()
@@ -41,29 +38,27 @@ def bfs():
             continue
 
         for dx, dy in directions:
-            # 각 구슬 이동
             nrx, nry, rc = move(crx, cry, dx, dy)
             nbx, nby, bc = move(cbx, cby, dx, dy)
 
             # 파란 구슬이 구멍에 빠지면 실패
             if nbx == ox and nby == oy:
-                continue
+                continue  # queue에 있는 다른 경우들도 마저 확인해야 함
 
             # 빨간 구슬이 구멍에 빠지면 성공
             if nrx == ox and nry == oy:
                 return cnt + 1
 
-            # 두 구슬이 같은 위치면 더 많이 이동한 구슬을 한 칸 뒤로
+            # 두 구슬이 같은 위치라면 더 많이 움직인 구슬이 한 칸 뒤로 감
             if nrx == nbx and nry == nby:
-                if rc > bc:  # 빨간이 더 이동 -> 빨간을 뒤로
+                if rc > bc:
                     nrx -= dx
                     nry -= dy
-                else:  # 파란이 더 이동 -> 파란을 뒤로
+                else:
                     nbx -= dx
                     nby -= dy
 
-            state = (nrx, nry, nbx, nby)
-            if state not in visited:
+            if (state := (nrx, nry, nbx, nby)) not in visited:
                 visited.add(state)
                 queue.append((nrx, nry, nbx, nby, cnt + 1))
 
