21a6f0f8d298669a63a5f9129ac8a278ab68cbb6
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-04-04 17:40:08 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,83 +1,40 @@
-import sys
+def solution():
+    N, M, x, y, K = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    com = list(map(int, input().split()))
 
-dice = [0] * 6
-map_info = []
-row, col, x, y, cmd = map(int, sys.stdin.readline().split())
+    dice = [0] * 6  # 위0 앞1 오2 아3 뒤4 왼5
 
-for _ in range(row):
-    map_info.append(list(map(int, sys.stdin.readline().split())))
+    dr = [0, 0, 0, -1, 1]
+    dc = [0, 1, -1, 0, 0]
 
-def check_in_bound(x, y):
-    if 0 <= x < row and 0 <= y < col:
-        return True
+    def roll(d):
+        top, front, right, bottom, back, left = dice
+        if d == 1:  # 동
+            dice[2], dice[3], dice[5], dice[0] = top, right, bottom, left
+        elif d == 2:  # 서
+            dice[5], dice[0], dice[2], dice[3] = top, right, bottom, left
+        elif d == 3:  # 북
+            dice[3], dice[1], dice[0], dice[4] = front, top, back, bottom
+        elif d == 4:  # 남
+            dice[0], dice[4], dice[3], dice[1] = front, top, back, bottom
 
-for dir in (cmd_info := list(map(int, sys.stdin.readline().split()))):
-    if dir == 1:  # 동쪽
-        y += 1
-        if check_in_bound(x, y):
-            temp = dice[0]
-            dice[0] = dice[3]
-            dice[3] = dice[5]
-            dice[5] = dice[2]
-            dice[2] = temp
-            print(dice[0])
-            if map_info[x][y] == 0:
-                map_info[x][y] = dice[5]
-            else:
-                dice[5] = map_info[x][y]
-                map_info[x][y] = 0
+    for c in com:
+        nx, ny = x + dr[c], y + dc[c]
 
-        else:
-            y -= 1
+        # 범위 벗어나면 무시
+        if not (0 <= nx < N and 0 <= ny < M):
             continue
-    elif dir == 2:  # 서쪽
-        y -= 1
-        if check_in_bound(x, y):
-            temp = dice[0]
-            dice[0] = dice[2]
-            dice[2] = dice[5]
-            dice[5] = dice[3]
-            dice[3] = temp
-            print(dice[0])
-            if map_info[x][y] == 0:
-                map_info[x][y] = dice[5]
-            else:
-                dice[5] = map_info[x][y]
-                map_info[x][y] = 0
-        else:
-            y += 1
-            continue
-    elif dir == 3:  # 북쪽
-        x -= 1
-        if check_in_bound(x, y):
-            temp = dice[0]
-            dice[0] = dice[4]
-            dice[4] = dice[5]
-            dice[5] = dice[1]
-            dice[1] = temp
-            print(dice[0])
-            if map_info[x][y] == 0:
-                map_info[x][y] = dice[5]
-            else:
-                dice[5] = map_info[x][y]
-                map_info[x][y] = 0
-        else:
-            x += 1
-            continue
-    else:  # 남쪽
-        x += 1
-        if check_in_bound(x, y):
-            temp = dice[0]
-            dice[0] = dice[1]
-            dice[1] = dice[5]
-            dice[5] = dice[4]
-            dice[4] = temp
-            print(dice[0])
-            if map_info[x][y] == 0:
-                map_info[x][y] = dice[5]
-            else:
-                dice[5] = map_info[x][y]
-                map_info[x][y] = 0
-        else:
-            x -= 1
-            continue
+
+        x, y = nx, ny
+        roll(c)
+
+        if board[x][y] == 0:  # 바닥 값이 0이라면
+            board[x][y] = dice[3]
+        else:   # 바닥 값이 0이 아니라면
+            dice[3] = board[x][y]
+            board[x][y] = 0
+
+        print(dice[0])
+
+solution()
