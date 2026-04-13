f876a1a957a9787cb89f8eaafe7e1663f2fe5786
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-04-13 11:32:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,37 @@
-from collections import deque
-input = open(0).readline
+from collections import deque 
+import sys 
+input = sys.stdin.readline
 
 N = int(input())
-maze = [list(map(int, input().strip())) for _ in range(N)]
-dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-cnt = 0
-house_cnt = []
+board = [list(input().rstrip()) for _ in range(N)]  # 단지정보 및 방문여부
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]  # 동0 남1 서2 뷱3
+homes = []  # 단지에 속하는 집 개수들 저장 
 
-def bfs(dx, dy):
-    house = 0
-    q = deque([(dx, dy)])
-    maze[dx][dy] = -1
+def bfs(x, y, num):
+    q = deque([(x, y)])
+    cnt = 1  # 같은 단지의 집 개수 
 
     while q:
-        x, y = q.popleft()
-        house += 1
-        for i in range(4):
-            nx, ny = x + dirs[i][0], y + dirs[i][1]
-            if 0 <= nx < N and 0 <= ny < N and maze[nx][ny] == 1:
+        rx, ry = q.popleft()
+        
+        for d in dirs:
+            nx, ny = rx + d[0], ry + d[1]
+            
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == '1':
+                cnt += 1
+                board[nx][ny] = '2'
                 q.append((nx, ny))
-                maze[nx][ny] = -1  # 방문처리
 
-    house_cnt.append(house)
+    homes.append(cnt)
+    return 
 
-for i in range(N):
-    for j in range(N):
-        if maze[i][j] == 1:
-            cnt += 1
-            bfs(i, j)
+for r in range(N):
+    for c in range(N):
+        if board[r][c] == '1':  # 방문하지 않은 집이라면
+            board[r][c] = '2'
+            bfs(r, c, board[r][c])
+            
 
-print(cnt)
-print(*sorted(house_cnt), sep='\n')
+print(len(homes))
+for h in sorted(homes):
+    print(h)
