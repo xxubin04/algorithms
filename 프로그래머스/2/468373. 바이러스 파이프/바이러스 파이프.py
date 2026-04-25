ab45c34d196aa51f8551eef361a7b5533f42ca33
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-04-25 23:20:47 +0900

    [level 2] Title: 바이러스 파이프, Time: 25.61 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
@@ -1,41 +1,28 @@
-# 최대한 많은 배양체에 바이러스 감염 
-# 한 종류의 파이프만 열 수 있음 
-    
+from itertools import product
+from collections import deque 
+
 def solution(n, infection, edges, k):
-    global graph, visited, max_infect
+    graph = {}
+    for s, e, p in edges:
+        for a, b in [(s, e), (e, s)]:
+            graph.setdefault(a, {})[b] = p
+    
+    def spread(infected, pipe_type):
+        q = deque(infected)
+        new_infected = set(infected)
+        while q:
+            node = q.popleft()
+            for end, p in graph.get(node, {}).items():
+                if p == pipe_type and end not in new_infected:
+                    new_infected.add(end)
+                    q.append(end)
+        return new_infected
     
     max_infect = 0
-    visited = [0] * (n+1)
-    graph = {}
-    
-    for s, e, p in edges:  # 시작점, 도착점, 파이프 종류
-        if s in graph:
-            graph[s][e] = p
-        else:
-            graph[s] = {e: p}
-            
-        if e in graph:
-            graph[e][s] = p
-        else:
-            graph[e] = {s: p}
-
-    def dfs(cnt, infected, pipe):
-        global max_infect
+    for actions in product([1, 2, 3], repeat=k):
+        infected = {infection}
+        for pipe_type in actions:
+            infected = spread(infected, pipe_type)
         max_infect = max(max_infect, len(infected))
-        if cnt == k:  # 최대 방문횟수에 도달했다면
-            return
-
-        for next_pipe in range(1, 4):
-            q = list(infected)
-            new_infected = set(infected)
-            for node in q:
-                for end, p in graph.get(node, {}).items():
-                    if p == next_pipe and end not in new_infected:
-                        new_infected.add(end)
-                        q.append(end)
-                        
-            if new_infected != infected:
-                dfs(cnt+1, new_infected, next_pipe)
-            
-    dfs(0, {infection}, -1)
+    
     return max_infect
