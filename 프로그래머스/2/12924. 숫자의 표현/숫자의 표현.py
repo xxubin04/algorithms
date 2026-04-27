09828344ac249c5deec2ad98eb71c3b1a154b28e
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-04-27 12:13:42 +0900

    [level 2] Title: 숫자의 표현, Time: 3.78 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,11 @@
 def solution(n):
     cnt = 0
-    start, end = 1, 1
-    while start <= n:
-        total = (start + end) * (end - start + 1) / 2
-        if total == n:
+    for i in range(1, n+1):
+        ssum = 0
+        while ssum < n:
+            ssum += i
+            i += 1
+        if ssum == n:
             cnt += 1
-            start += 1
-            end += 1
-        elif total < n:
-            end += 1
-        else:
-            start += 1
-    
+            
     return cnt
