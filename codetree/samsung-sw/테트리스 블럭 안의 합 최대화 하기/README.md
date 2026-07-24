978a42512d0c7dffc86c80b381c1fb53b80596e8
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-07-24 10:09:22 +0900

    [Passed] 테트리스 블럭 안의 합 최대화 하기 (131ms, 20MB)

---
+++ codetree/samsung-sw/테트리스 블럭 안의 합 최대화 하기/README.md
@@ -0,0 +1,9 @@
+# [테트리스 블럭 안의 합 최대화 하기](https://www.codetree.ai/frequent-problems/samsung-sw/problems/max-sum-of-tetris-block)
+
+**SW 역량 테스트** › 2017 상반기 오전 1번 문제
+
+| 항목 | 내용 |
+|---|---|
+| 난이도 | 12 |
+| 유형 | Simulation |
+| 경험치 | 0 XP |
---
+++ codetree/samsung-sw/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -0,0 +1,51 @@
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+visited = [[0] * M for _ in range(N)]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+ans = 0
+
+# DFS로 4칸 탐색
+def dfs(r, c, depth, total):
+    global ans
+    if depth == 4:
+        ans = max(ans, total)
+        return
+    for i in range(4):
+        nr, nc = r + dr[i], c + dc[i]
+        if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
+            visited[nr][nc] = 1
+            dfs(nr, nc, depth+1, total+board[nr][nc])
+            visited[nr][nc] = 0  #  백트래킹
+
+# ㅗ 모양 4가지
+T_shapes = [
+    [(0,0), (1,0), (2,0), (1,1)],
+    [(0,0), (1,0), (2,0), (1,-1)],
+    [(0,0), (0,1), (0,2), (1,1)],
+    [(0,0), (0,1), (0,2), (-1,1)]
+]
+
+for r in range(N):
+    for c in range(M):
+        # DFS
+        visited[r][c] = 1
+        dfs(r, c, 1, board[r][c])
+        visited[r][c] = 0
+
+        # ㅗ 모양
+        for shape in T_shapes:
+            total = 0
+            valid = True
+            for ddr, ddc in shape:
+                nr, nc = r + ddr, c + ddc
+                if 0 <= nr < N and 0 <= nc < M:
+                    total += board[nr][nc]
+                else:
+                    valid = False
+                    break
+            if valid:
+                ans = max(ans, total)
+
+print(ans)
