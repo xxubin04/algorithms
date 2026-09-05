4d02897056e24397029cebdf1c09afcd3a71fa6d
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-09-05 22:21:53 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,43 @@
 def solution(progresses, speeds):
-    time = []
     answer = []
+    days = []
     
     for p, s in zip(progresses, speeds):
-        if (cal := (100 - p)) % s == 0:  # 나누어 떨어진다면
-            time.append(cal // s)
-        else:
-            time.append(cal // s + 1)  # 나누어 떨어지지 않는다면 
+        days.append((100 - p - 1) // s + 1)
     
-    std = time[0]  # 배포 시기에 가장 앞에 위치한 기능의 필요한 시간
-    cnt = 1  # 한 번에 배포될 수 있는 개수
-    for i in range(1, len(time)):
-        if time[i] <= std:
+    std = days[0]
+    cnt = 0
+    for d in days:
+        if d <= std:
             cnt += 1
         else:
             answer.append(cnt)
+            std = d
             cnt = 1
-            std = time[i]
-    
+            
     answer.append(cnt)
+    
     return answer
+    
+#     time = []
+#     answer = []
+    
+#     for p, s in zip(progresses, speeds):
+#         if (cal := (100 - p)) % s == 0:  # 나누어 떨어진다면
+#             time.append(cal // s)
+#         else:
+#             time.append(cal // s + 1)  # 나누어 떨어지지 않는다면 
+    
+#     std = time[0]  # 배포 시기에 가장 앞에 위치한 기능의 필요한 시간
+#     cnt = 1  # 한 번에 배포될 수 있는 개수
+#     for i in range(1, len(time)):
+#         if time[i] <= std:
+#             cnt += 1
+#         else:
+#             answer.append(cnt)
+#             cnt = 1
+#             std = time[i]
+    
+#     answer.append(cnt)
+#     return answer
     
