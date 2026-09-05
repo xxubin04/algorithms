3a1d4a69906da5ccffcc6bff9bdc61c31fb2dbd2
정유빈 <127945869+xxubin04@users.noreply.github.com>
2026-09-05 20:27:02 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 0.01 ms
+메모리: 11.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 25일 11:42:11
+2026년 09월 05일 20:26:57
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,14 @@
 def solution(cl):
+    answer = 1
     clothes = {}
     
     for c in cl:
-        if (type := c[1]) in clothes.keys():
-            clothes[type].append(c[0])
+        if c[1] in clothes:
+            clothes[c[1]].append(c[0])
         else:
-            clothes[type] = [c[0]]
+            clothes[c[1]] = [c[0]]
+        
+    for v in clothes.values():
+        answer *= (len(v)+1)
     
-    comb = 1
-    for v in clothes.values():  # 각 종류마다 존재하는 의상 개수
-        comb *= (len(v)+1)
-    
-    return comb-1
+    return answer - 1  # 아무것도 안 입는 경우(1) 빼기
